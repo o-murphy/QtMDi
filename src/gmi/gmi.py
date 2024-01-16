9023360c0fb641cfc980fe862614c6c45cd2e703
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-01-16 14:49:45 +0200

    add symbols font

--- src/gmi/gmi.py
+++ src/gmi/gmi.py
@@ -6,7 +6,10 @@ from qtpy import QtWidgets
 
 import qtawesome
 
-FONT_DIR = os.path.join(os.path.dirname(__file__), 'font')
+
+SEARCH_DIR = os.path.dirname(__file__)
+FONT_DIR = os.path.join(SEARCH_DIR, 'font')
+SYMBOLS_DIR = os.path.join(SEARCH_DIR, 'symbols')
 
 
 _BUILT_IN_FONTS = (
@@ -34,12 +37,51 @@ _BUILT_IN_FONTS = (
         "gmi-2tone",
         "MaterialIconsTwoTone-Regular.otf",
         "MaterialIconsTwoTone-Regular-charmap.json",
-    )
+    ),
 )
+
+
+def create_symbols_prefix(filename):
+    try:
+        return f"gms-{os.path.splitext(filename)[0].split('-')[-1]}"
+    except Exception:
+        return f"gms-other"
+
+
+_BUILT_IN_SYMBOLS = [
+    (
+        create_symbols_prefix(filename),
+        filename,
+        "charmap.json",
+    ) for filename in os.listdir(SYMBOLS_DIR) if filename.endswith(".ttf")
+]
+
+print(_BUILT_IN_SYMBOLS)
+
+# _BUILT_IN_SYMBOLS = (
+#     (
+#         "gms-outlined",
+#         "material-symbols-outlined.ttf",
+#         "charmap.json",
+#     ),
+#     (
+#         "gms-outlined",
+#         "material-symbols-rounded.ttf",
+#         "charmap.json",
+#     ),
+#     (
+#         "gms-outlined",
+#         "material-symbols-sharp.ttf",
+#         "charmap.json",
+#     )
+# )
 
 
 def load(app: QtWidgets.QApplication):
     if app == QtWidgets.QApplication.instance():
+
+        for symbols in _BUILT_IN_SYMBOLS:
+            qtawesome.load_font(*symbols, SYMBOLS_DIR)
 
         for font in _BUILT_IN_FONTS:
             qtawesome.load_font(*font, FONT_DIR)
