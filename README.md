7f149477d11aa6ed9b62c686c7e6b3833606bb0e
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-01-16 16:15:46 +0200

    add LICENSE

---
+++ README.md
@@ -0,0 +1,3 @@
+# QtMDI (Qt Material Design Icons)
+
+qtawesome extension with the latest variable icon fonts for Material Symbols (Python 3)
--- src/create_charmap.py
+++ src/create_charmap.py
@@ -1,17 +1,20 @@
+"""Creates a *-charmap.json files for built-in fonts"""
+
 import os
 import re
 import json
-from gmi import FONT_DIR
+from qtmdi import FONT_DIR  # pylint: disable=import-error
+
 
 REGEX_SEARCH = r'(\w+) ([a-f\d]+)'
 
 codepoints_files = [file for file in os.listdir(FONT_DIR) if file.endswith('.codepoints')]
 for filename in codepoints_files:
     filepath = os.path.join(FONT_DIR, filename)
-    with open(filepath, 'r') as fp:
+    with open(filepath, 'r', encoding='utf-8') as fp:
         content = fp.read()
 
     output = dict((key, f"0x{value}") for key, value in re.findall(REGEX_SEARCH, content))
     dst = os.path.join(FONT_DIR, os.path.splitext(filename)[0]) + '-charmap.json'
-    with open(dst, 'w') as fp:
+    with open(dst, 'w', encoding='utf-8') as fp:
         json.dump(output, fp)
