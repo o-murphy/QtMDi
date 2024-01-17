eeaa90e216046ef90e377fb365e5f5c7dfb6fb4d
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-01-17 16:05:39 +0200

    add pyinstaller hook

--- src/qtmdi/__pyinstaller/hook-qtmdi.py
+++ src/qtmdi/__pyinstaller/hook-qtmdi.py
@@ -4,6 +4,7 @@ Hook for PyInstaller
 collects package data if PyInstaller packaging requires celpy
 """
 
+# pylint: disable=import-error
 from PyInstaller.utils.hooks import collect_data_files
 
 datas = collect_data_files('qtmdi')
